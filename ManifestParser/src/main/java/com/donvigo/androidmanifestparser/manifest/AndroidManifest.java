17f20bde405ad0dfb8efe6f73750c2b1d89c0c24
Adorilson Bezerra <adorilson@ppgsc.ufrn.br>
2015-06-27 13:50:52 -0300

    Add tests and AndroidManifest.getManifestFromXML(...) method

--- ManifestParser/src/main/java/com/donvigo/androidmanifestparser/manifest/AndroidManifest.java
+++ ManifestParser/src/main/java/com/donvigo/androidmanifestparser/manifest/AndroidManifest.java
@@ -1,8 +1,15 @@
 package com.donvigo.androidmanifestparser.manifest;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
+
+import com.donvigo.androidmanifestparser.xml.XmlHandler;
 
 /**
  * Created by vgaidarji on 12.03.14.
@@ -38,11 +45,34 @@ public class AndroidManifest {
     public void setApplication(ApplicationEntry application) {
         this.application = application;
     }
+    
+    public static AndroidManifest getManifestFromXML(String pathToFile){
+		String manifestXmlString = null;
+		AndroidManifest androidManifest = null;
+		try {
+			manifestXmlString = parseManifestFile(pathToFile);
+			androidManifest = new XmlHandler<AndroidManifest>().parse(AndroidManifest.class, manifestXmlString);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+        return androidManifest;
+	}
+    
+    private static String parseManifestFile(String path) throws IOException {
+		
+		FileInputStream entrada = new FileInputStream(path);
+		InputStreamReader entradaFormatada = new InputStreamReader(entrada);
+		BufferedReader br = new BufferedReader(entradaFormatada);
+		
+		String linha = br.readLine();
+		StringBuffer buffer = new StringBuffer();
+		while(linha != null) {
+		   buffer.append(linha);
+		   linha = br.readLine();
+		}
+		
+		br.close();
+		return buffer.toString();
+	}
 }
-
-
-
-
-
-
-
---
+++ ManifestParser/src/main/java/com/donvigo/androidmanifestparser/tests/Main.java
@@ -0,0 +1,34 @@
+package com.donvigo.androidmanifestparser.tests;
+
+import java.util.Iterator;
+
+import com.donvigo.androidmanifestparser.manifest.ActivityEntry;
+import com.donvigo.androidmanifestparser.manifest.AndroidManifest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String filePath = args[0];
+		System.out.println("Reading " + filePath);
+		
+		AndroidManifest manifest = AndroidManifest.getManifestFromXML(filePath);
+		//AndroidManifest manifest = AndroidManifest.getManifestFromAPK(filePath);
+		
+		System.out.println("PackageName: " + manifest.getPackageName());
+		/*
+		if (manifest.getUsesSdk()!=null){
+			System.out.println("MinSdkVersion: " + manifest.getUsesSdk().getMinSdkVersion());
+			System.out.println("TargetSdkVersion: " + manifest.getUsesSdk().getTargetSdkVersion());
+		}
+		System.out.println("Label: " + manifest.getApplication().getLabel());
+		
+		System.out.println("Activities: " + manifest.getApplication().getActivities());
+		
+		for (Iterator<ActivityEntry> iterator = manifest.getApplication().getActivities().iterator(); iterator.hasNext();) {
+			ActivityEntry entry = (ActivityEntry) iterator.next();
+			System.out.println(entry.getName() + " - " + entry.getTheme());
+			
+		}
+		*/
+	}
+}
